3008b2ea8d475faf42da26ab99692254148df488
Arinsz <arin.sz@hotmail.com>
2025-02-07 17:05:53 +0100

    test

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,14 @@ import Hero from "./components/FirstPage/Hero";
 
 import HowItWorks from "./components/FirstPage/HowItWorks";
 import AppDownload from "./components/FirstPage/AppDownload";
+import Testimonials from "./components/FirstPage/Testimonials";
 
 export default function Home() {
   return (
     <AuthProvider>
       <main className="min-h-screen">
         <Hero />
+        <Testimonials />
         <HowItWorks />
         <AppDownload />
       </main>

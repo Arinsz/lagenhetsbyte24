4c2ee0085d0bf8843527d08d03369717680324d4
Arinsz <arin.sz@hotmail.com>
2025-02-09 23:17:30 +0100

    test

--- app/components/firstPage/Hero.tsx
+++ app/components/firstPage/Hero.tsx
@@ -82,21 +82,23 @@ export default function Hero() {
             >
               Ingen kostnad för medlemskap!
             </motion.p>
-            <div className="flex flex-row space-x-3 mb-10 w-full">
-              <Input
-                type="text"
-                placeholder="Sök efter stad eller område"
-                className="w-2/4 min-w-[220px] text-black"
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
-              />
-              <Button
-                size="lg"
-                variant="secondary"
-                className="h-auto w-20 sm:w-10 md:w-28 lg:w-32 max-w-20"
-              >
-                <Search className="mr-2 h-3 w-3 sm:h-4 sm:w-4" /> Sök
-              </Button>
+            <div className="flex flex-row space-x-3 mb-10 w-full max-w-md">
+              <div className="relative flex-grow">
+                <Input
+                  type="text"
+                  placeholder="Sök efter stad eller område"
+                  className="w-full text-black h-auto pr-10"
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <Button
+                  size="lg"
+                  variant="secondary"
+                  className="absolute inset-y-0 right-0 flex items-center px-3 h-full"
+                >
+                  <Search className="h-4 w-4" />
+                </Button>
+              </div>
             </div>
             <ul className="space-y-4 max-w-md mb-8">
               <motion.li

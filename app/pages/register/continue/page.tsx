6a9a549d94bc4313ba4a1c271a79b3715ba55c11
Arinsz <arin.sz@hotmail.com>
2025-02-14 17:02:16 +0100

    test

--- app/pages/register/continue/page.tsx
+++ app/pages/register/continue/page.tsx
@@ -2,8 +2,24 @@
 
 // Import necessary components
 import MapView from "../../../components/Login-Register/MapView";
-import ContinueRegisterForm from "../../../components/Login-Register/FilterSideBar";
+import ContinueRegisterForm from "../../../components/Login-Register/FilterSidebar";
+import { useEffect } from "react";
 
 export default function Page() {
+  useEffect(() => {
+    // Hide the footer
+    const footer = document.querySelector("footer");
+    if (footer) {
+      footer.style.display = "none";
+    }
+
+    // Show the footer when the component is unmounted
+    return () => {
+      if (footer) {
+        footer.style.display = "";
+      }
+    };
+  }, []);
+
   return <MapView />;
 }
